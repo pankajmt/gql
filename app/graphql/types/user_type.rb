cd0cbb91af7eccaba4493eacb8ba2ef36b1c7e72
Pankaj Tolani <pankaj.tolani@gmail.com>
2021-02-15 13:41:42 +1100

    query and mutations

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,5 +5,11 @@ module Types
     field :name, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :posts, [Types::PostType], null: true
+    field :posts_count, Integer, null: true
+
+    def posts_count
+      object.posts.size
+    end
   end
 end
